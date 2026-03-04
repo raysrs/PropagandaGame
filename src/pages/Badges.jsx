48a6da27160e49f45003164cb7135f034a33cc7f
zoepsomas <zoepsomas@gmail.com>
2026-03-04 10:10:55 -0800

    adding badges

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -1,6 +1,28 @@
+import BadgeandBlurb from "../functions/BadgeandBlurb";
+
 function Badges() {
   return (
-    <></>
+    <>
+      <div className="bg-orange-500 text-bold text-center h-[50px] text-2xl font-bold py-3">
+          My Badges
+      </div>
+      <div className="bg-blue-400 p-6">
+        <BadgeandBlurb
+          badgename="badge name"
+          blurb="blurb">
+        </BadgeandBlurb>
+        <BadgeandBlurb
+          badgename="example"
+          blurb="in progress">
+        </BadgeandBlurb>
+        <BadgeandBlurb
+          badgename="example"
+          blurb="in progress">
+        </BadgeandBlurb>
+
+      </div>
+    
+    </>
   )
 }
 
