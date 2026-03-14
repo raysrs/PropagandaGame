6d394ef436dd672cb8320e4a29ada2acc9c75f41
zoepsomas <zoepsomas@gmail.com>
2026-03-14 13:01:41 -0700

    adding centering

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,24 +4,29 @@ import placeholder from "../assets/placeholder.jpg";
 
 function About() {
   return (
-    <div className="bg-blue-400 p-4">
-      <div className="text-4xl font-bold text-center rounded-lg bg-blue-600">About Us!</div>
-      <div className="flex items-stretch">
-        <AboutCard
-        name="Zoe"
-        picture={zoe}
-        blurb="Hi">
-        </AboutCard>
+    <div className="bg-sky-400 p-4 h-[1000px] font-serif">
+      <div className="text-4xl font-bold text-center rounded-lg bg-amber-400">About Us!</div>
+      <div className="flex flex-wrap justify-center">
+        <div>
+          <AboutCard
+          name="Zoe"
+          picture={zoe}
+          blurb="Hi">
+          </AboutCard>
+        </div>
 
-        <AboutCard
-        name="Ray"
-        picture={placeholder}
-        blurb="Hello">
-        </AboutCard>
-      </div>
-      <div className="bg-pink-300 p-4 m-4 rounded-lg">
-        <div className="text-center text-2xl font-semibold">Our Mission</div>
-        <div className="text-center">Blah, blah, blah.</div>
+        <div>
+          <AboutCard
+          name="Ray"
+          picture={placeholder}
+          blurb="Hello">
+          </AboutCard>
+        </div>
+      
+        <div className="bg-pink-400 p-4 m-4 rounded-lg grow">
+          <div className="text-center text-2xl font-semibold">Our Mission</div>
+          <div className="text-center">Blah, blah, blah.</div>
+        </div>
       </div>
       
     
