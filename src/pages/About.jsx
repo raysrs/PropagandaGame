2d746fe9b068c5d4bdb2f2a3f14fb3f5890685ba
zoepsomas <zoepsomas@gmail.com>
2026-03-16 10:20:02 -0700

    adding exit buttons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import AboutCard from "../components/AboutCard";
 import zoe from "../assets/zoe.jpg";
 import placeholder from "../assets/placeholder.jpg";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <div className="bg-sky-400 p-4 h-[1000px] font-serif">
+    <div className="bg-sky-400 p-4 h-dvh font-serif">
       <div className="text-4xl font-bold text-center rounded-lg bg-amber-400">About Us!</div>
       <div className="flex flex-wrap justify-center">
         <div>
@@ -28,7 +29,9 @@ function About() {
           <div className="text-center">Blah, blah, blah.</div>
         </div>
       </div>
-      
+      <Link to="/" className="p-2 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold absolute right-4 bottom-4 w-[70px] text-center text-base">
+        Exit
+      </Link>
     
     
     </div>
