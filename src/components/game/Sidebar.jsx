a622f711bbcfd2c839cabdd35eaa711fc411d805
zoepsomas <zoepsomas@gmail.com>
2026-03-20 10:11:29 -0700

    sidebar updates

--- src/components/game/Sidebar.jsx
+++ src/components/game/Sidebar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-import placeholder from "../../assets/placeholder.jpg";
+import patricia from "../../assets/patricia.jpg"
 
 function Sidebar({likes}) {
   return(
     <div className="w-xs h-dvh p-2 border-2 font-serif bg-sky-400 text-xl">
-      <img src={placeholder} alt="Patricia Politician"></img>
+     <div className="h-[150px] w-[150px] overflow-hidden flex mx-[70px] mt-2">
+       <img className="h-full w-full object-cover object-center rounded-full" src={patricia} alt="Patricia Politician"></img>
+       </div>
       <div className="text-center">
         Patricia Politician
       </div>
@@ -17,9 +19,24 @@ function Sidebar({likes}) {
         </div>
       </div>
 
-      <Link to="/" className="p-2 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold absolute left-4 bottom-4 w-[70px] text-center text-base">
-        Exit
-      </Link>
+       <div className="w-[300px] flex justify-center my-8">
+       <Link to="/" className="p-2 mt-[40px] mx-8 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold text-center text-xl text-base w-[150px]">
+         Home
+       </Link>
+     </div>
+
+
+     <div className="w-[300px] flex justify-center my-8">
+       <Link to="/badges" className="p-2 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold text-center text-xl text-base w-[150px]">
+         View Badges
+       </Link>
+     </div>
+     <div className="w-[300px] flex justify-center my-8"> 
+       <Link to="/about" className="p-2 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold text-center text-xl text-base w-[150px]">
+         About Us
+       </Link>
+     </div>
+
     </div>
   );
 };
