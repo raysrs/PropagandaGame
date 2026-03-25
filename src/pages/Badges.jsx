0aa224039f65559c459e3a247535d3821a6596d4
zoepsomas <zoepsomas@gmail.com>
2026-03-25 09:28:37 -0700

    styling badges

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -1,5 +1,10 @@
 import BadgeandBlurb from "../components/BadgeandBlurb";
 import { Link } from "react-router-dom";
+import cliffhangers from "../assets/cliffhangers.jpg";
+import collocations from "../assets/collocations.jpg";
+import opposites from "../assets/opposites.jpg";
+import essentialist from "../assets/essentialist.jpg";
+import disposition from "../assets/disposition.jpg";
 
 function Badges() {
   return (
@@ -10,22 +15,27 @@ function Badges() {
       <div className="p-6 flex flex-wrap">
         <BadgeandBlurb
           badgename="Level 1 - Noun Subjectivity + Collocations"
+          image={collocations}
           blurb="Subjective nouns convey an opinion using their collocations (the associations people have with words). In contrast, objective nouns are more factual.">
         </BadgeandBlurb>
         <BadgeandBlurb
           badgename="Level 2 - Linguistic Cliffhangers"
+          image={cliffhangers}
           blurb="Linguistic Cliffhangers are phrases that are incomplete, leaving the audience to fill in the blank with their own biases. The technique makes the speaker seem less biased while taking advantage of people’s preexisting prejudices.">
         </BadgeandBlurb>
         <BadgeandBlurb
           badgename="Level 3 - Language of Opposites"
+          image={opposites}
           blurb="Politicians use the language of opposites to increase the believability and perceived danger of their chosen enemy. It makes the speaker seem more trustworthy, as it appears that they are telling their audience what they know currently.">
         </BadgeandBlurb>
         <BadgeandBlurb
           badgename="Level 4 - Implied Disposition"
+          image={disposition}
           blurb="When disposition is implied, this means that the speaker is implying that a certain person or group may do something. However, the likelihood of the subject actually completing the action is not specified.">
         </BadgeandBlurb>
         <BadgeandBlurb
           badgename="Level 5 - Essentialist Placeholders"
+          image={essentialist}
           blurb="Essentialist placeholders are the mechanisms we use to understand how certain people belong in specific groups without actually knowing why or how.">
         </BadgeandBlurb>
         <Link to="/" className="p-2 bg-pink-400 rounded-lg hover:bg-fuchsia-500 hover:font-semibold absolute right-4 bottom-4 w-[70px] text-center text-base">
