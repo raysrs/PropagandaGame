1d7fee115cf5bf6d0540b94bf2f015b0b64eee60
zoepsomas <zoepsomas@gmail.com>
2026-04-22 09:00:38 -0700

    adding empty badge styling

--- src/components/BadgeandBlurb.jsx
+++ src/components/BadgeandBlurb.jsx
@@ -16,9 +16,9 @@ export function BlankBadgeandBlurb(){
   return(
     <div className="flex items-stretch">
       <div className="h-[210px] w-[210px] overflow-hidden rounded-full text-center inline-block align-middle">
-        <div className="z-0 h-full w-full object-cover object-center rounded-full bg-gray-300" />
+        <div className="z-0 h-full w-full object-cover object-center rounded-full bg-pink-400" />
       </div>
-      <div className="bg-lime-500 w-[300px] h-[210px] p-4 m-4" />
+      <div className="bg-lime-500 w-[300px] h-[210px] p-4 m-4"> Please complete the level to earn this badge.</div>
     </div>
   )
 }
